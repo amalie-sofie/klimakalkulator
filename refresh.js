d29af1a0796cbe7d6e5c5515ca2b61e5e6a15001
Skog1 <117902204+Skog1@users.noreply.github.com>
2022-11-10 20:06:47 +0000

    sum

--- refresh.js
+++ refresh.js
@@ -3,11 +3,11 @@ const outputElement2 = document.querySelector("#output2");
 
 function refresh() {
   // Logikk for å oppdatere kalkulator
-  const streaming = ((get("streamtv") * 100) / 1000) * 52);
+  const sum = ((get("streamtv") * 100) / 1000) * 52);
   const sum2 = ((get("test1") * 100) / 1000) * 52;
 
   const sum3 = sum + sum2;
 
-  outputElement.textContent = streaming;
+  outputElement.textContent = sum;
   outputElement2.textContent = sum3;
 }
