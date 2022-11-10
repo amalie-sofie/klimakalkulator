ad02b742cc12730c3a66f9ebfb87e72108ea4a5a
Skog1 <117902204+Skog1@users.noreply.github.com>
2022-11-10 20:59:04 +0000

    Flere utregninger

--- refresh.js
+++ refresh.js
@@ -6,7 +6,7 @@ const outputElement5 = document.querySelector("#output5");
 const outputElement6 = document.querySelector("#output5");
 
 function refresh() {
-// utregning stream
+// UTREGNING STREAM
   const sum = ((get("streamtv") * 100) / 1000) * 52;
   const sum2 = ((get("streammusikk") * 36) / 1000) * 52;
 
@@ -14,9 +14,35 @@ function refresh() {
 
   outputElement.textContent = sum3;
  
-  // utregning skylagring
+  // UTREGNING SKYLAGRING
 
-  const sum4 = ((get("skylagring") * 0.86) *442) / 1000;
+  const sum4 = ((get("skylagring") * 0.81) *442) / 1000;
  
   outputElement2.textContent = sum4;
+
+  // UTREGNING INTERNETTBRUK
+
+  const sum5 = ((get("epost") * 20) / 1000) * 52;
+  const sum6 = ((get("internett") * 52) / 1000);
+
+  const sum7 = sum5 + sum6;
+
+  outputElement3.textContent = sum7;
+
+  // UTREGNING MOROSAME TING
+
+  const sum8 = ((get("spillkonsoll") * 50) / 1000) * 52;
+  const sum9 = ((get("spillpc") * 90) / 1000) * 52;
+  const sum10 = ((get("kino") * 2400) / 1000) * 52;
+
+  const sum10 = sum8 + sum9 + sum10;
+
+  outputElement4.textContent = sum10;
+
+
+  // TOTALT UTSLIPP
+
+  const sum50 = sum3 + sum4 + sum7 + sum10;
+
+  outputElement6.textContent = sum50;
 }
