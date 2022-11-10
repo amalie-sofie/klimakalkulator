d1e4a0514c4da1230718b62c245fe7aab70b7c9c
Skog1 <117902204+Skog1@users.noreply.github.com>
2022-11-10 20:10:34 +0000

    kalk

--- refresh.js
+++ refresh.js
@@ -2,7 +2,7 @@ const outputElement = document.querySelector("#output");
 const outputElement2 = document.querySelector("#output2");
 
 function refresh() {
-  // Logikk for å oppdatere kalkulator
+
   const sum = ((get("streamtv") * 100) / 1000) * 52);
   const sum2 = ((get("test1") * 100) / 1000) * 52;
 
