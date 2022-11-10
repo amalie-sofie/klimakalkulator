33deb2162dff765874b30aaba92f936211ab2c4a
Skog1 <117902204+Skog1@users.noreply.github.com>
2022-11-10 20:48:33 +0000

    Utregning skylagring

--- refresh.js
+++ refresh.js
@@ -6,12 +6,17 @@ const outputElement5 = document.querySelector("#output5");
 const outputElement6 = document.querySelector("#output5");
 
 function refresh() {
-
+// utregning stream
   const sum = ((get("streamtv") * 100) / 1000) * 52;
   const sum2 = ((get("streammusikk") * 36) / 1000) * 52;
 
   const sum3 = sum + sum2;
 
   outputElement.textContent = sum3;
+ 
+  // utregning skylagring
+
+  const sum4 = ((get("skylagring") * 0.86) *442) / 1000;
+ 
   outputElement2.textContent = sum4;
 }
